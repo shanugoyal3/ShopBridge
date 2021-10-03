3f03daf8525a6325ab34c16af62de8e90ecb5af3
user <user@SHANU-PC>
2021-10-03 15:35:23 +0530

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopBridge.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name Field is Required")]
+        public string  Name { get; set; }
+        [Required(ErrorMessage = "Description Field is Required")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Price Field is Required")]
+        public decimal Price { get; set; }
+
+
+        public Category Category { get; set; }
+        [ForeignKey("CategoryId")]
+        public byte  CategoryId { get; set; }
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using ShopBridge.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,16 +28,36 @@ namespace ShopBridge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>
+          (o => o.UseSqlServer(Configuration.
+          GetConnectionString("ProductAppCon")));
+
+            //Enable CORS means disabling the security to allow the requests from multiple domains
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+            //we are adding one morething like json serializes as default to reciece data in json format
+            services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+            .Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
             services.AddControllers();
         }
 
+
+        
+        
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext db)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            db.Database.EnsureCreated();
 
             app.UseHttpsRedirection();
 
